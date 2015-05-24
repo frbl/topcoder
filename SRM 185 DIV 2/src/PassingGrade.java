6c1d9402c2e02546d4c65f7e11b1cdaedbdddca4
Frank Blaauw <frank.blaauw@gmail.com>
2015-05-24 22:22:29 +0200

    more problems

--- SRM 185 DIV 2/src/PassingGrade.java
+++ SRM 185 DIV 2/src/PassingGrade.java
@@ -1,19 +1,20 @@
 import java.util.*;
 import java.math.*;
+
 import static java.lang.Math.*;
 
 public class PassingGrade {
-	
-	public int pointsNeeded(int[] pointsEarned, int[] pointsPossible, int finalExam) {
-		int total = 0	;
-		int total_possible = finalExam;
-		for(int i = 0 ; i < pointsEarned.length; i++ ) {
-			total += pointsEarned[i];
-			total_possible += pointsPossible[i];
-		}
 
-		int r = (int) Math.ceil((total_possible * 0.65)) - total;
-		if(r < 0) return 0;
-		return r > finalExam ? -1 : r;
-	}
+    public int pointsNeeded(int[] pointsEarned, int[] pointsPossible, int finalExam) {
+        int total = 0;
+        int total_possible = finalExam;
+        for (int i = 0; i < pointsEarned.length; i++) {
+            total += pointsEarned[i];
+            total_possible += pointsPossible[i];
+        }
+
+        int r = (int) Math.ceil((total_possible * 0.65)) - total;
+        if (r < 0) return 0;
+        return r > finalExam ? -1 : r;
+    }
 }
